46e4670337440d056005e54f7dc39090af0cb454
yalatan <nikitinanf@gmail.com>
2019-03-20 19:41:42 +0300

    first decision

--- src/queue.js
+++ src/queue.js
@@ -2,24 +2,40 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if (this.size() == this.maxSize){
+			throw new Error('Уупс!');
+		}
+	return	this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if(this.heap.parentNodes.length == 0){throw new Error('Уупс!');}
+		else {
+		return this.heap.pop();}
+		
 	}
 
-	size() {
-
+	size() {		
+		let l = this.heap.parentNodes.length;
+		if( l == 0) {return 0}
+		else{
+		if (this.heap.parentNodes[0].parent != null){
+		l = l + 1;
+		}	
+		return 	l;	
+		}
 	}
 
 	isEmpty() {
 		
-	}
+		if(this.heap.parentNodes.length == 0 || this.heap.root == null ){return true}	
+		else {return false};
+}
 }
 
 module.exports = PriorityQueue;
